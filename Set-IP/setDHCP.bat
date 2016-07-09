beb2d17b032d861049b23ebc7d7dda4740d5a493
pksm <paulamoraes95@gmail.com>
2016-07-09 08:42:28 -0300

    Added support to older Windows versions
    Similar modifications to staticIP.bat

--- Set-IP/setDHCP.bat
+++ Set-IP/setDHCP.bat
@@ -29,10 +29,39 @@ if '%errorlevel%' NEQ '0' (
     CD /D "%~dp0"
 :-------------------------------------- 
 ECHO CHANGE ETHERNET INTERFACE FROM STATIC TO DHCP 
-netsh interface ip set address Ethernet dhcp
-ECHO Success!
-echo.
-ECHO Current configuration...
-echo.
-netsh interface ip show address Ethernet
-PAUSE 
+goto inputAdapterName
+
+:inputAdapterName
+	echo.
+	echo A list of your current network settings will be shown below. 
+	echo.
+	echo Search for your ethernet adapter's name 
+	echo.
+	echo E.g. 'Adapter Ethernet Ethernet' or 'Adapter Ethernet Local Area Connection'
+	echo.
+	pause
+	ipconfig
+	echo.
+	SET /P netName=From the list above...Please input your Ethernet Adapter name: 
+	GOTO checkAdapterName
+
+:checkAdapterName
+	echo.
+	netsh interface ip show address %netName% &&  GOTO confirmAdapter 2>nul GOTO inputAdapterName
+
+:confirmAdapter
+	echo. 
+	SET /P confirm=Is the above adaptor correct[y/n]?
+	IF /I {%confirm%}=={y} (
+		GOTO success
+	)ELSE (
+		goto inputAdapterName
+	)
+
+:success	
+	netsh interface ip set address %netName% dhcp && ECHO Success!
+	echo.
+	ECHO Current configuration...
+	echo.
+	netsh interface ip show address %netName%
+	PAUSE 

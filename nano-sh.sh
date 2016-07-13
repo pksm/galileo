66375dfe132699cd68914fd2ad828a8ab3a16ab7
pksm <paulamoraes95@gmail.com>
2016-07-13 18:55:17 -0300

    Create nano-sh.sh
    Adding shell syntax highlight support to Nano editor

---
+++ nano-sh.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Adding shell syntax highlight support to Nano editor
+# 
+FILE=/usr/share/nano/sh.nanorc
+cp $FILE $FILE.old
+while read line
+do
+	sed -i 's;\\<;\\b;g' $FILE
+	sed -i 's/\\>/\\b/g' $FILE
+done < $FILE
+echo include "/usr/share/nano/sh.nanorc" > ~/.nanorc

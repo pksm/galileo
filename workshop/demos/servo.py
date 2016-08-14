99136b6548a63a576900a6ae3188f2abc71a815f
pksm <paulamoraes95@gmail.com>
2016-08-13 22:54:29 -0300

    Update servo.py

--- workshop/demos/servo.py
+++ workshop/demos/servo.py
@@ -7,7 +7,7 @@ x.period_ms(7)
 x.enable(True)
 time.sleep( 3 )
 while True:
-  for i in arange(0.1, 0.36, 0.05): #best range for micro servo SG90 on Intel Galileo
+  for i in arange(0.1, 0.38, 0.05): #best range for micro servo SG90 on Intel Galileo Gen2 
     print i
     x.write(i)
     time.sleep(3)

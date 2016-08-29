514e0b01d3df45199e9c8946471828c60dcb9c99
pksm <paulamoraes95@gmail.com>
2016-08-29 09:28:38 -0300

    Update blink.py
    add comments

--- workshop/demos/blink.py
+++ workshop/demos/blink.py
@@ -1,3 +1,9 @@
+'''
+   'hello world!' of hardware projects
+   Blink LED on pin #8 of Intel Galileo Gen 2
+   
+   Keyboard interrupt exception (CTRL+C) treated, so quiting the program should return the message 'Exiting program...' on terminal
+'''
 import mraa as m, time as t
 led = m.Gpio(8)
 led.dir(m.DIR_OUT)
@@ -5,7 +11,7 @@ led.dir(m.DIR_OUT)
 try:
    while(1):
         led.write(1)
-        t.sleep(1) #delay de 200ms
+        t.sleep(1) #delay 1 second
         led.write(0)
         t.sleep(1)
 
